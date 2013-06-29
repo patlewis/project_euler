8dc88c01c498a7ffc36500ca0206b2d0e0a00b46
Pat Lewis <pat.lewis.vt@gmail.com>
2013-06-29 00:57:52 -0700

    Fixed problem 1, added supplementary materials

--- prob1.c
+++ prob1.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <math.h>
+
+/** COMPILER NOTE: This program MUST be compiled with the -lm flag **/
+
 /*
  * Project Euler problem 1--Multiples of 3 and 5.  The problem description from 
  * the website reads:
@@ -8,55 +12,41 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
  *
  *
- * The idea behind the solution is to keep a running sum of the numbers that
- * qualify.  Since all we're doing is additions, we don't really have to worry 
- * about counting floating-point operations.  We first start with 3, because 
- * that's the first number that qualifies.  We increment the current number by
- * three and then add that number.  Once all of the multiples of three have 
- * been added, we go through all the multiples of five.  If a number is both a 
- * multiple of three and five, then it is skipped during the addition of threes.
- * This way we can avoid branches so that the algorithm runs a lot faster.    
- * We sacrifice a bit of coding elegance for a tad of efficiency. 
+ * The idea behind the solution is to exploit the summation formula 
+ * that Gauss originally came up with.  We use SUM = (n/2)(a + b) where n is the
+ * overall number of items, a is the starting number, and b is the ending 
+ * number.  For a more robust solution, we would normally use variables for a
+ * and b, and somehow find them.  For this situation, we already know that the 
+ * starting number will be 3, the ending number will be 999, and the number of 
+ * numbers will be 333.  This is just some basic mental math.  
+ * 
+ * For the sake of this particular problem, we will also use a little set 
+ * theory.  In general, the exclusive OR (XOR) of sets A and B is the union of
+ * A and B minus the intersection of A and B.  So the set of all numbers that is
+ * divisible by 3 or 5 is the set of all numbers divisible by three unioned with
+ * the set of all numbers divisible by 5 minus the set of all numbers divisible
+ * by 3 and 5.  Since there's really no way to algorithmically figure this out
+ * because of the arbitrary problem description, it is just hardcoded in.
  */
+
+int sum(int, int);
 
  int main(void)
  {
- 	int sum3 = 0;
- 	int x = 0;
- 	//first do multiples of three
- 	while(x < 1000)
- 	{
- 		x += 3; sum3 += x;	printf("%d\n", x);//3
- 		x += 3;	sum3 += x;	printf("%d\n", x);//6
- 		x += 3; sum3 += x;	printf("%d\n", x);//9
- 		x += 3; sum3 += x;	printf("%d\n", x);//12
- 		x += 3; printf("skip %d\n", x);//skip the multiple of 5
- 	}
- 	//now do multiples of 5
- 	int sum5;
- 	x = 5;
- 	while(x < 1000)
- 	{
- 		sum5 += x;
- 		x += 5;
- 	}
- 	///////////////////////////////////////////////////////////////////////////
- 	printf("\n\n");
- 	int sum31;
- 	sum31 = 0;
- 	for (x = 0; x < 1000; x++)
- 	{
- 		if ( (x%3 == 0) && (x%5 != 0))
- 		{
- 			printf("%d\n", x);
- 			sum31 += x;
- 		}
- 	}
- 	if(sum3 != (sum31)) printf("FUCK(3): %d \t %d\n", sum3, sum31);
- 	else
- 	{
- 		int sum = sum3 + sum5;
-		printf("Sum of all multiples of 3 and 5 up to 1000:  %d\n", sum);
- 	}
+ 	int total = sum(999,3) + sum(999,5) - sum(999,15);
+ 	printf("Sum = %d\n", total);
  	return 0;
+ }
+
+/*
+ * b is the max number, m is the multiple
+ */
+ int sum(int b, int m)
+ {
+ 	int n = (int)(b / m); //number of numbers to use
+ 	// Since we have to account for all numbers being less than b, we have to 
+ 	// massage the numbers a little bit.  We have to use m * int((1+b)/m) to 
+ 	// ensure this condition, otherwise we might end up with numbers greater
+ 	// than b.
+ 	return (int)(n * m * (1 + n) / 2);
  }
