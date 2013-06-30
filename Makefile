11e4bdf8578f96c16915b21b3ab270e36c0c65f4
Pat Lewis <pat.lewis.vt@gmail.com>
2013-06-29 21:16:06 -0700

    Added problem 3

--- Makefile
+++ Makefile
@@ -7,6 +7,8 @@ p1:
 	$(CC) $(CFLAGS) prob1.c -o sol1
 p2:
 	$(CC) $(CFLAGS) prob2.c -o sol2 -lm
+p3:	
+	$(CC) $(CFLAGS) prob3.c -o sol3 -lm 
 
 clean:
 	rm -f *.o sol*
---
+++ prob3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+
+/*
+ * Project Euler Problem 3: Prime Factorization
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 
+ *
+ * After a little bit of research, it looks like there isn't any "good" 
+ * algorithm for this. The best I could really do would be to do a trial 
+ * division, where you just divide a number n by each integer up to sqrt(n).
+ * This one is a little different though, because as soon as I find a factor, I
+ * divide the number I'm testing by that factor so that I don't accidently find
+ * that factor again.
+ *
+ */
+
+int factors(unsigned long);
+
+ int main(int argc, char* argv[])
+ {
+ 	// The commented stuff was just for test values.  I don't really care
+ 	// about the test values anymore.
+ 	// if (argc != 2)
+ 	// {
+ 	// 	printf("Usage: sol3 <int> where <int> is an integer to find the prime");
+ 	// 	printf(" factorization of.\n");
+ 	// 	return 0;
+ 	// }
+ 	//long arg = atoi(argv[1]);
+ 	printf("Greatest Prime Factor: %d\n", factors(600851475143));
+ 	return 0;
+ }
+
+ int factors(unsigned long num)
+ {
+ 	long numm =  num; //for negatives
+ 	assert(numm > 2); //or else it's pretty pointless
+ 	int greatestfactor = 1;
+ 	int x;
+ 	int STOP = floor(sqrt(numm));
+ 	for(x = 2; x < STOP; x++)
+ 	{
+ 		while(numm % x == 0) 	//this x is a factor
+ 		{
+ 			greatestfactor = x;
+ 			numm /= x; 		//to find duplicates of this factor
+ 		}
+ 	}
+ 	if (greatestfactor == 1) greatestfactor = numm; //if already prime
+ 	return greatestfactor;
+ }
