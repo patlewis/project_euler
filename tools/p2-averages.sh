bbe15aa489165fd9e940d15db8b2063fb56979bb
Pat Lewis <pat.lewis.vt@gmail.com>
2013-08-14 17:25:02 -0700

    Fixed decimal errors related to timing bug in problem 2

--- tools/p2-averages.sh
+++ tools/p2-averages.sh
@@ -9,8 +9,8 @@ IAVG=$(echo "$IAVG + $(awk '/Iterative/ {print $4}' temp.fle)" | bc)
 MAVG=$(echo "$MAVG + $(awk '/Multiplicative/ {print $4}' temp.fle)" | bc)
 done
 
-IAVG=$(echo "scale=6; $IAVG / 1000" | bc)
-MAVG=$(echo "scale=6; $MAVG / 1000" | bc)
+IAVG=$(echo "scale=9; $IAVG / 1000" | bc)
+MAVG=$(echo "scale=9; $MAVG / 1000" | bc)
 
 echo "Iterative solution average:      $IAVG s"
 echo "Multiplicative solution average: $MAVG s"
